e3b2a138090f303b3c9a43ff08d0ce0a2861bd1b
johnny <hansonjohnny648@gmail.com>
2026-05-14 17:01:34 +0000

    update

---
+++ vars/frontendPipeline.groovy
@@ -0,0 +1,13 @@
+def call(Map config = [:]) {
+    fullPipeline(
+        service:         'frontend',
+        appDir:          'frontend',
+        k8sDir:          "k8s/frontend",
+        sonarProjectKey:  config.sonarProjectKey  ?: 'cloud-native-frontend',
+        sonarProjectName: config.sonarProjectName ?: 'cloud-native-frontend',
+        ecrCredId:        config.ecrCredId        ?: 'ECR_REPO_FRONTEND',
+        gitRepoName:      config.gitRepoName       ?: 'nimb-platform',
+        gitUserName:      config.gitUserName       ?: 'hansonjohnny',
+        awsRegion:        config.awsRegion         ?: 'us-east-2'
+    )
+}
--- vars/updateK8sDeployment.groovy
+++ vars/updateK8sDeployment.groovy
@@ -1,20 +1,20 @@
-def call(String service, String repoUri, String ecrRepoName) {
-    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
-        // checkout the platform repo
-        sh """
-            git clone https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimb-platform.git
-        """
-        dir("nimb-platform/k8s/${service}") {
-            sh """
-                git config user.email "hansonjohnny648@gmail.com"
-                git config user.name "hansonjohnny"
+// def call(String service, String repoUri, String ecrRepoName) {
+//     withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+//         // checkout the platform repo
+//         sh """
+//             git clone https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimb-platform.git
+//         """
+//         dir("nimb-platform/k8s/${service}") {
+//             sh """
+//                 git config user.email "hansonjohnny648@gmail.com"
+//                 git config user.name "hansonjohnny"
 
-                sed -i "s|image:.*|image: ${repoUri}${ecrRepoName}:${BUILD_NUMBER}|g" deployment.yaml
+//                 sed -i "s|image:.*|image: ${repoUri}${ecrRepoName}:${BUILD_NUMBER}|g" deployment.yaml
 
-                git add deployment.yaml
-                git commit -m "ci: update ${service} image to build ${BUILD_NUMBER}"
-                git push https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimb-platform.git HEAD:main
-            """
-        }
-    }
-}
+//                 git add deployment.yaml
+//                 git commit -m "ci: update ${service} image to build ${BUILD_NUMBER}"
+//                 git push https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimb-platform.git HEAD:main
+//             """
+//         }
+//     }
+// }
