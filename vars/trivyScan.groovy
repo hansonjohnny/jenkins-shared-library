3997938d584e66737868405ca7d6af833c96f2ae
johnny <hansonjohnny648@gmail.com>
2026-05-14 23:35:00 +0000

    update trivy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,21 +1,21 @@
 def call(String appDir, String imageName) {
     sh "mkdir -p ${WORKSPACE}/reports/trivy"
 
-    // Filesystem scan
-    dir(appDir) {
-        sh """
-            trivy fs --severity LOW,MEDIUM --format table .
-        """
-        sh """
-            trivy fs \
-                --severity HIGH,CRITICAL \
-                --exit-code 0 \
-                --format json \
-                --output ${WORKSPACE}/reports/trivy/fs-scan.json .
-        """
+    if (appDir) {
+        dir(appDir) {
+            sh """
+                trivy fs --severity LOW,MEDIUM --format table .
+            """
+            sh """
+                trivy fs \
+                    --severity HIGH,CRITICAL \
+                    --exit-code 0 \
+                    --format json \
+                    --output ${WORKSPACE}/reports/trivy/fs-scan.json .
+            """
+        }
     }
 
-    // Image scan (only if imageName provided)
     if (imageName) {
         sh """
             trivy image --severity LOW,MEDIUM --format table ${imageName}
