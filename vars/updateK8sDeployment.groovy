210088ec77193a80c9a718fb4ba14ac0848eedf6
johnny <hansonjohnny648@gmail.com>
2026-05-19 13:16:14 +0000

    update deployment

--- vars/updateK8sDeployment.groovy
+++ vars/updateK8sDeployment.groovy
@@ -2,7 +2,7 @@ def call(String service, String accountId, String ecrRepoName, String region) {
     withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
         sh "git clone https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimbs-platform.git"
 
-        dir('nimb-platform') {
+        dir('nimbs-platform') {
             sh """
                 git config user.email "jenkins@nimbus.com"
                 git config user.name "Jenkins"
@@ -12,7 +12,7 @@ def call(String service, String accountId, String ecrRepoName, String region) {
 
                 git add helm/values/${service}.yaml
                 git commit -m "ci: update ${service} image to build ${BUILD_NUMBER}"
-                git push https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimb-platform.git HEAD:main
+                git push https://hansonjohnny:\${GITHUB_TOKEN}@github.com/hansonjohnny/nimbs-platform.git HEAD:main
             """
         }
     }
