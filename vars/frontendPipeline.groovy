ef6aadae2dbbe8f0414b592645a268c7d49ad4c7
johnny <hansonjohnny648@gmail.com>
2026-05-20 00:39:15 +0000

    update deployment

--- vars/frontendPipeline.groovy
+++ vars/frontendPipeline.groovy
@@ -6,7 +6,7 @@ def call(Map config = [:]) {
         sonarProjectKey:  config.sonarProjectKey  ?: 'cloud-native-frontend',
         sonarProjectName: config.sonarProjectName ?: 'cloud-native-frontend',
         ecrCredId:        config.ecrCredId        ?: 'ECR_REPO_FRONTEND',
-        gitRepoName:      config.gitRepoName       ?: 'nimb-platform',
+        gitRepoName:      config.gitRepoName       ?: 'nimbs-platform',
         gitUserName:      config.gitUserName       ?: 'hansonjohnny',
         awsRegion:        config.awsRegion         ?: 'us-east-2'
     )
